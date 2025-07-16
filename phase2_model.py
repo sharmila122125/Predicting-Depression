2dbec0492f46a687e760a9f82fdcfb353bd06040
sharmila122125 <sharmila122125@gmail.com>
2025-07-16 12:29:43 +0530

    Track model with Git LFS and add code

---
+++ phase2_model.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+class DepressionModel(nn.Module):
+    def __init__(self, input_dim):
+        super(DepressionModel, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.network(x)
